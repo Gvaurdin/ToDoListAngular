35fa418916ee2c99b925a9ab00ae7a0033376fa2
Gvaurdin <gvaurdin777@yandex.ru>
2024-10-28 17:47:01 +0300

    создал todoList на Angular 18

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,52 @@
+import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+export interface ToDoItem {
+  id: number;
+  task: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgFor, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'todolist-app';
+  todoList: ToDoItem[] = [];
+  newTask: string = '';
+
+  addTask(): void {
+    if (this.newTask.trim() !== '') {
+      const newToDoItem: ToDoItem = {
+        id: Date.now(),
+        task: this.newTask,
+        completed: false
+      }
+      this.todoList.push(newToDoItem);
+      console.log(newToDoItem);
+      this.newTask = '';
+    }
+  }
+
+  deleteTask(id: number): void {
+    if (id != null) {
+      this.todoList = this.todoList.filter(item => item.id !== id);
+    }
+  }
+
+  toggleCompleted(index: number): void {
+    console.log(index);
+    this.todoList[index].completed = !this.todoList[index].completed;
+    console.log(this.todoList);
+  }
+
+  deleteCompletedTasks(): void {
+    this.todoList = this.todoList.filter(item => !item.completed);
+  }
 }
